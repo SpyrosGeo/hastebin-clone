963a8b4c5c5bcabd585c3e3535a127c92f06afc9
SpyrosGeo <xarodasimba@gmail.com>
2021-09-05 14:29:01 +0300

    added basic css styling

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const app = express()
 const PORT = 5000
 
 app.set('view engine',"ejs")
-
+app.use(express.static('public'))
 app.get('/',(req,res)=>{
-    res.send("Hello fuck!")
+    const code = `Welcome to WasteBin
+    
+Use the commands in the top right corner
+to create a new file to share with others`
+    res.render('code-display',{code:code,lineNumbers:code.split('\n').length})
 })
 
 
