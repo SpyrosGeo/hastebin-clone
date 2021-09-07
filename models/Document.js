3e57af56dbf4b0faf4316f184e873394b6061294
SpyrosGeo <xarodasimba@gmail.com>
2021-09-07 22:53:23 +0300

    removed node_modules and also finished the app

---
+++ models/Document.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+const documentScheme = new mongoose.Schema({
+    value:{
+        type:String,
+        required:true
+        
+    }
+})
+
+export default mongoose.model("Document",documentScheme)
--- server.js
+++ server.js
@@ -1,26 +1,65 @@
-import express from 'express'
+import express from "express";
+// const mongoose = require('mongoose')
+import mongoose from "mongoose";
+import Document from "./models/Document.js";
+import dotenv from 'dotenv'
+dotenv.config()
+const app = express();
+const PORT = 5000;
 
-const app = express()
-const PORT = 5000
+const username =process.env.MONGODB_USERNAME
+const password  =process.env.MONGODB_USERNAME
 
-app.set('view engine',"ejs")
-app.use(express.static('public'))
-app.get('/',(req,res)=>{
-    const code = `Welcome to WasteBin
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(
+  `mongodb+srv://${username}:${password}@cluster0.mrs6z.mongodb.net/failbin?retryWrites=true&w=majority`,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  }
+);
+
+app.get("/", (req, res) => {
+  const code = `Welcome to WasteBin
     
 Use the commands in the top right corner
-to create a new file to share with others`
-    res.render('code-display',{code:code,lineNumbers:code.split('\n').length})
-})
+to create a new file to share with others`;
+  res.render("code-display", {
+    code: code,
+    language:'plaintext'
+  });
+});
 
 //Create New Document
-app.get('/new',(req,res)=>{
-    res.render("new")
-})
+app.get("/new", (req, res) => {
+  res.render("new");
+});
 
-app.post('/save',(req,res)=>{
-    
-})
-app.listen(PORT,()=>{
-    console.log(`Listening at port: ${PORT}`)
-})
+app.post("/save", async (req, res) => {
+    const value = req.body.value
+  try {
+    const document = await Document.create({ value });
+    res.redirect(`/${document.id}`);
+  } catch (error) {
+    res.render("new", { value });
+  }
+});
+
+//specific note route
+
+app.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const document = await Document.findById(id);
+    res.render("code-display", { code: document.value });
+  } catch (error) {
+    res.redirect("/");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening at port: ${PORT}`);
+});
